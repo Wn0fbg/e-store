3c44ba0296fa07b8179c25bad68fb10fa7551c37
Wn0fbg <temakozlovofficiall@gmail.com>
2026-03-03 21:29:26 +0300

    debaging

--- server/middlewares/authModdleware.js
+++ server/middlewares/authModdleware.js
@@ -3,18 +3,23 @@ import ErrorHandler from "./errorMiddleware.js";
 import database from "../database/db.js";
 
 export const isAuthenticated = catchAsyncErrors(async (req, res, next) => {
-  const { token } = req.user.id;
+  const { token } = req.cookies;
 
   if (!token) {
     return next(new ErrorHandler("Please login to access this resource", 401));
   }
 
-  const user = await database.query("SELECT * FROM users WHERE id = $1", [
-    token.id,
-  ]);
+  const user = await database.query(
+    "SELECT * FROM users WHERE user_id = $1 LIMIT 1",
+    [token.id],
+  );
+
+  if (!user.rows.length) {
+    return next(new ErrorHandler("User not found", 401));
+  }
 
   req.user = user.rows[0];
-  next(0);
+  next();
 });
 
 export const authorizeRoles = (...roles) => {
@@ -22,10 +27,10 @@ export const authorizeRoles = (...roles) => {
     if (!roles.includes(req.user.role)) {
       return next(
         new ErrorHandler(
-          `Role: ${req.user.role} is not allowed to access to this resourse`,
+          `Role: ${req.user.role} is not allowed to access this resource`,
         ),
       );
     }
-    next();
+    next(); // добавлено
   };
 };
