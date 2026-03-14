7b000c14131998d3e737242bacade4af424c547e
Wn0fbg <temakozlovofficiall@gmail.com>
2026-03-14 20:19:50 +0300

    finished debaging

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -3,65 +3,13 @@ import ErrorHandler from "../middlewares/errorMiddleware.js";
 import database from "../database/db.js";
 
 import path from "path";
-import fs from "fs";
-import util from "util";
-const mkdir = util.promisify(fs.mkdir);
-const stat = util.promisify(fs.stat);
-const renameFile = util.promisify(fs.rename);
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+import { stat, mkdir, rename as renameFile } from "fs/promises";
 import { v4 as uuidv4 } from "uuid";
 
-// export const createProduct = catchAsyncErrors(async (req, res, next) => {
-//   const { name, description, price, category, stock } = req.body;
-//   const created_by = req.user.id;
-
-//   if (!name || !description || !price || !category || !stock) {
-//     return next(
-//       new ErrorHandler("Please provide complete product details", 400),
-//     );
-//   }
-
-//   let uploadedImages = [];
-
-//   if (req.files && req.files.images) {
-//     const images = Array.isArray(req.files.images)
-//       ? req.files.images
-//       : [req.files.images];
-
-//     for (const image of images) {
-//       const result = await cloudinary.uploader.upload(image.tempFilePath, {
-//         folder: "Eccomerce_Product_Images",
-//         width: 1000,
-//         crop: "scale",
-//       });
-
-//       uploadedImages.push({
-//         url: result.secure_url,
-//         public_id: result.public_id,
-//       });
-//     }
-//   }
-
-//   const product = await database.query(
-//     `INSERT INTO products
-//       (name, description, price, category, stock, images, created_by)
-//       VALUE ($1, $2, $3, $4, $5, $6, $7) RETURNING *`,
-//     [
-//       name,
-//       description,
-//       price,
-//       category,
-//       stock,
-//       JSON.stringify(uploadedImages),
-//       created_by,
-//     ],
-//   );
-
-//   res.status(201).json({
-//     success: true,
-//     message: "Product created successfuly",
-//     product: product.rows[0],
-//   });
-// });
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export const createProduct = catchAsyncErrors(async (req, res, next) => {
   const { name, description, price, category, stock } = req.body;
@@ -74,7 +22,6 @@ export const createProduct = catchAsyncErrors(async (req, res, next) => {
   }
 
   let uploadedImages = [];
-
   const imagesDir = path.resolve(__dirname, "..", "uploads", "product_images");
 
   try {
