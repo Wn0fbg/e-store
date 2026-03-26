0bb4dc51ceb32bde5c922fd0519a0771156668a3
Wn0fbg <temakozlovofficiall@gmail.com>
2026-03-26 14:13:35 +0300

    created updateOrderStatus and deleteOrder

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -236,3 +236,93 @@ export const fetchMyOrders = catchAsyncErrors(async (req, res, next) => {
   });
 });
 
+export const fetchAllOrders = catchAsyncErrors(async (req, res, next) => {
+  const result = await database.query(`
+    SELECT o.*,
+    COALESCE(
+      json_agg(
+        json_build_object(
+          'order_item_id', oi.id,
+          'order_id', oi.order_id,
+          'product_id', oi.product_id,
+          'quantity', oi.quantity,
+          'price', oi.price,
+          'image', oi.image,
+          'title', oi.title
+        )
+      ) FILTER (WHERE oi.id IS NOT NULL), '[]'
+    ) AS order_items,
+    json_build_object(
+      'full_name', s.full_name,
+      'state', s.state,
+      'city', s.city,
+      'country', s.country,
+      'address', s.address,
+      'pincode', s.pincode,
+      'phone', s.phone
+    ) AS shipping_info
+    FROM orders o
+    LEFT JOIN order_items oi ON o.id = oi.order_id
+    LEFT JOIN shipping_info s on o.id = s.order_id
+    GROUP BY o.id, s.id
+  `);
+
+  res.status(200).json({
+    success: true,
+    message: "All orders fetched",
+    orders: result.rows,
+  });
+});
+
+export const updateOrderStatus = catchAsyncErrors(async (req, res, next) => {
+  const { status } = req.body;
+
+  if (!status) {
+    return next(new ErrorHandler("Provide a valid status for order.", 400));
+  }
+
+  const { orderId } = req.params;
+  const result = await database.query(
+    `
+    SELECT * FROM orders WHERE id = $1
+  `,
+    [orderId],
+  );
+
+  if (result.rows.length === 0) {
+    return next(new ErrorHandler("Invalid order ID.", 404));
+  }
+
+  const updateOrder = await database.query(
+    `
+    UPDATE orders SET order_status = $1 WHERE id = $2 RETURNING *
+  `,
+    [status, orderId],
+  );
+
+  res.status(200).json({
+    success: true,
+    message: "Order status updated",
+    updatedOrder: updateOrder.rows[0],
+  });
+});
+
+export const deleteOrder = catchAsyncErrors(async (req, res, next) => {
+  const { orderId } = req.params;
+  const result = await database.query(
+    `
+    DELETE FROM orders WHERE id = $1 RETURNING *
+  `,
+    [orderId],
+  );
+
+  if (result.rows.length === 0) {
+    return next(new ErrorHandler("Invalid order ID.", 404));
+  }
+
+  res.status(200).json({
+    success: true,
+    message: "Order deleted.",
+    order: result.rows[0],
+  });
+});
--- server/router/orderRoutes.js
+++ server/router/orderRoutes.js
@@ -3,6 +3,9 @@ import {
   placeNewOrder,
   fetchSingleOrder,
   fetchMyOrders,
+  fetchAllOrders,
+  updateOrderStatus,
+  deleteOrder,
 } from "../controllers/orderController.js";
 import {
   isAuthenticated,
@@ -14,5 +17,23 @@ const router = express.Router();
 router.post("/new", isAuthenticated, placeNewOrder);
 router.get("/:orderId", isAuthenticated, fetchSingleOrder);
 router.get("/orders/me", isAuthenticated, fetchMyOrders);
+router.get(
+  "/admin/getall",
+  isAuthenticated,
+  authorizeRoles("Admin"),
+  fetchAllOrders,
+);
+router.put(
+  "/admin/update/:orderId",
+  isAuthenticated,
+  authorizeRoles("Admin"),
+  updateOrderStatus,
+);
+router.delete(
+  "/admin/delete/:orderId",
+  isAuthenticated,
+  authorizeRoles("Admin"),
+  deleteOrder,
+);
 
 export default router;
