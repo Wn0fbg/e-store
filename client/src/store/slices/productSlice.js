bc2fda582fcd5783bda883d0dd61c0b6bead5df4
Wn0fbg <temakozlovofficiall@gmail.com>
2026-04-06 21:19:28 +0300

    start debaging productSlice

--- client/src/store/slices/productSlice.js
+++ client/src/store/slices/productSlice.js
@@ -2,6 +2,39 @@ import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import { axiosInstance } from "../../lib/axios";
 import { toast } from "react-toastify";
 
+export const fetchAllProducts = createAsyncThunk(
+  "/product/fetchAll",
+  async (
+    {
+      availability = "",
+      price = "0-100000",
+      category = "",
+      rating = "",
+      search = "",
+      page = "1",
+    },
+    thunkAPI,
+  ) => {
+    try {
+      const params = new URLSearchParams();
+
+      if (category) params.append("category", category);
+      if (price) params.append("price", price);
+      if (search) params.append("search", search);
+      if (rating) params.append("rating", rating);
+      if (availability) params.append("availability", availability);
+      if (page) params.append("page", page);
+
+      const res = await axiosInstance.get(`/product?${params.toString()}`);
+      return res.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(
+        error.responce.data.message || "Failet to fetch products",
+      );
+    }
+  },
+);
+
 const productSlice = createSlice({
   name: "product",
   initialState: {
@@ -16,7 +49,12 @@ const productSlice = createSlice({
     isPostingReview: false,
     productReviews: [],
   },
-  extraReducers: (builder) => {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchAllProducts.pending, (state) => {})
+      .addCase(fetchAllProducts.fulfilled, (state) => {})
+      .addCase(fetchAllProducts.rejected, (state) => {});
+  },
 });
 
 export default productSlice.reducer;
