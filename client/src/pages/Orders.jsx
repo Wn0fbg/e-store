ef1009a38b7f41602fb6096db1401a8877b8a1b5
Wn0fbg <temakozlovofficiall@gmail.com>
2026-04-14 22:32:22 +0300

    start functional for Orders page

--- client/src/pages/Orders.jsx
+++ client/src/pages/Orders.jsx
@@ -4,6 +4,41 @@ import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 
 const Orders = () => {
+  const [statusFilter, setStatusFilter] = useState("all");
+  const { myOrders } = useSelector((state) => state.order);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    // dispatch()
+  }, [dispatch]);
+
+  const filtetOrders = myOrders.filter(
+    (order) => statusFilter === "all" || order.order_status === statusFilter,
+  );
+
+  const getStatusIcon = (status) => {
+    switch (key) {
+      case "Processing":
+        return <Package className="size-5 text-yellow-500" />;
+        break;
+
+      case "Shipped":
+        return <Truck className="size-5 text-blue-500" />;
+        break;
+
+      case "Delivered":
+        return <CheckCircle className="size-5 text-green-500" />;
+        break;
+
+      case "Cancelled":
+        return <XCircle className="size-5 text-red-500" />;
+        break;
+
+      default:
+        break;
+    }
+  };
+
   return <></>;
 };
 
