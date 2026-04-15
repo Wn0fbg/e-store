f9ed63b95cb96abad67b850ba285e6e094701c74
Wn0fbg <temakozlovofficiall@gmail.com>
2026-04-15 20:09:09 +0300

    starte functional for Orders page

--- client/src/pages/Orders.jsx
+++ client/src/pages/Orders.jsx
@@ -1,10 +1,17 @@
 import React, { useEffect, useState } from "react";
-import { Filter, Package, Truck, CheckCircle, XCircle } from "lucide-react";
+import {
+  Filter,
+  Package,
+  Truck,
+  CheckCircle,
+  XCircle,
+  Link,
+} from "lucide-react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 
 const Orders = () => {
-  const [statusFilter, setStatusFilter] = useState("all");
+  const [statusFilter, setStatusFilter] = useState("All");
   const { myOrders } = useSelector((state) => state.order);
   const dispatch = useDispatch();
 
@@ -13,7 +20,7 @@ const Orders = () => {
   }, [dispatch]);
 
   const filtetOrders = myOrders.filter(
-    (order) => statusFilter === "all" || order.order_status === statusFilter,
+    (order) => statusFilter === "All" || order.order_status === statusFilter,
   );
 
   const getStatusIcon = (status) => {
@@ -62,6 +69,14 @@ const Orders = () => {
     }
   };
 
+  const statusArray = [
+    "All",
+    "Processing",
+    "Shipped",
+    "Delivered",
+    "Cancelled",
+  ];
+
   const { authUser } = useSelector((state) => state.auth);
   const navigateTo = useNavigate();
 
@@ -81,6 +96,145 @@ const Orders = () => {
           </div>
 
           {/* Status Filter */}
+          <div className="glass-card p-4 mb-8">
+            <div className="flex items-center space-x-4 flex-wrap">
+              <div className="flex items-center space-x-4">
+                <Filter className="size-5 text-primary" />
+                <span className="font-medium">Filter by states:</span>
+              </div>
+              <div className="flex flex-wrap gap-2">
+                {statusArray.map((status) => {
+                  return (
+                    <button
+                      key={status}
+                      onClick={() => setStatusFilter(status)}
+                      className={`px-4 py-2 rounded-lg font-medium transition-all capitalize ${
+                        statusFilter === status
+                          ? "gradient-primary text-primary-foreground"
+                          : "glass-card hover:glow-on-hover text-foreground"
+                      }`}
+                    >
+                      {status}
+                    </button>
+                  );
+                })}
+              </div>
+            </div>
+          </div>
+
+          {/* Orders list */}
+          {filtetOrders.length === 0 ? (
+            <div className="text-center glass-panel max-w-md mx-auto">
+              <Package className="size-16 text-muted-foreground mx-auto mb-4" />
+              <h2 className="text-xl font-semibold text-foreground mb-6">
+                No orders found
+              </h2>
+              <p className="text-muted-foreground">
+                {statusFilter === "All"
+                  ? "You haven't placed any orders yet."
+                  : `No orders with status "${statusFilter}" found.`}
+              </p>
+            </div>
+          ) : (
+            <div className="space-y-6">
+              {filtetOrders.map((order) => {
+                return (
+                  <div key={order.id} className="glass-card p-6 ">
+                    {/* Order header */}
+                    <div
+                      className={`flex flex-col md:flex-row md:items-center 
+                    md:justify-between space-y-4 md:space-y-0`}
+                    >
+                      <h3 className="text-lg font-semibold from-foreground mb-1">
+                        Orders #{order.id}
+                      </h3>
+                      <p className="text-muted-foreground">
+                        Placed on{" "}
+                        {new Date(order.created_at).toLocaleDateString()}
+                      </p>
+                      <div className="flex items-center space-x-4">
+                        <div className="flex items-center space-x-2"></div>
+                        {getStatusIcon(order.order_status)}
+                        <span
+                          className={`px-3 py-1 rounded text-sm font-medium capitalize ${getStatusColor(
+                            order.order_status,
+                          )}`}
+                        >
+                          {order.order_status}
+                        </span>
+                      </div>
+
+                      <div className="text-right">
+                        <p className="text-sm text-muted-foreground">Total</p>
+                        <p className="text-xl font-bold text-primary">
+                          ${order.total_price}
+                        </p>
+                      </div>
+                    </div>
+
+                    {/* Order items */}
+                    <div className="space-x-4">
+                      {order?.order_items?.map((item) => {
+                        <div
+                          key={item.product_id}
+                          className="flex items-center space-x-4 p-4 bg-secondary/50 rounded-lg"
+                        >
+                          <img
+                            src={"/avatar-holder.avif"}
+                            // src={item.image}
+                            alt={item.title}
+                            className="size-16 object-cover rounded-lg"
+                          />
+                          <div className="flex min-w-0">
+                            <h4 className="font-medium text-foreground truncate">
+                              {item.title}
+                            </h4>
+                            <p className="text-sm text-muted-foreground">
+                              Quantity: {item.quantity}
+                            </p>
+                          </div>
+
+                          <div className="text-right">
+                            <p className="font-semibold text-foreground">
+                              ${item.price}
+                            </p>
+                          </div>
+                        </div>;
+                      })}
+                    </div>
+
+                    {/* Order actions */}
+                    <div>
+                      <div className="flex flex-wrap gap-3 mt-6 pt-6 border-t border-[hsla(var(--glass-border))]">
+                        <button className="px-4 py-2 glass-card hover:glow-on-hover animate-smooth text-sm">
+                          View Details
+                        </button>
+                        <button className="px-4 py-2 glass-card hover:glow-on-hover animate-smooth text-sm">
+                          Track Order
+                        </button>
+                        {order.status === "Delivered" && (
+                          <>
+                            <button className="px-4 py-2 glass-card hover:glow-on-hover animate-smooth text-sm">
+                              Write Review
+                            </button>
+                            <button className="px-4 py-2 glass-card hover:glow-on-hover animate-smooth text-sm">
+                              Reorder
+                            </button>
+                          </>
+                        )}
+
+                        {/* {order.status === "Processing" && (
+                          <button className="px-4 py-2 glass-card hover:glow-on-hover animate-smooth text-sm text-destructive">
+                            Cancel Order
+                          </button>
+                        )} */}
+                      </div>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          )}
         </div>
       </div>
     </>
--- client/src/store/slices/orderSlice.js
+++ client/src/store/slices/orderSlice.js
@@ -2,6 +2,34 @@ import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import { axiosInstance } from "../../lib/axios";
 import { toast } from "react-toastify";
 
+export const fetchMyOrders = createAsyncThunk(
+  "order/orders/me",
+  async (_, thunkAPI) => {
+    try {
+      const res = await axiosInstance.get("/order/orders/me");
+      return res.data.myOrders;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.response.data.message);
+    }
+  },
+);
+
+export const placeOrder = createAsyncThunk(
+  "order/new",
+  async (data, thunkAPI) => {
+    try {
+      const res = await axiosInstance.post("/order/new", data);
+      toast.success(res.data.message);
+      return res.data;
+    } catch (error) {
+      toast.error(
+        error.response.data.message || "Failedt to place order, try again.",
+      );
+      return thunkAPI.rejectWithValue(error.response.data.message);
+    }
+  },
+);
+
 const orderSlice = createSlice({
   name: "order",
   initialState: {
@@ -12,8 +40,14 @@ const orderSlice = createSlice({
     orderStep: 1,
     paymentIntent: "",
   },
-  reducers: {},
-  extraReducers: (builder) => {},
+  reducers: {
+    toggleOrderStep(state) {
+      state.orderStep = 1;
+    },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(fetchMyOrders.pending, (state) => {});
+  },
 });
 
 export default orderSlice.reducer;
