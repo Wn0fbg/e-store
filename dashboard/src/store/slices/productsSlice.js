77dd34d1c146217d0f8e17f9e87dff928371cb34
Wn0fbg <temakozlovofficiall@gmail.com>
2026-04-29 22:41:55 +0300

    started Products page

--- dashboard/src/store/slices/productsSlice.js
+++ dashboard/src/store/slices/productsSlice.js
@@ -1,4 +1,10 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { axiosInstance } from "../../lib/axios";
+import { toast } from "react-toastify";
+import {
+  toggleCreateProdcutModal,
+  toggleUpdateProductModal,
+} from "./extraSlice";
 
 const productSlice = createSlice({
   name: "product",
@@ -7,7 +13,103 @@ const productSlice = createSlice({
     products: [],
     totalProducts: 0,
   },
-  reducers: {},
+  reducers: {
+    createProductRequest(state) {
+      state.loading = true;
+    },
+    createProductSuccess(state, action) {
+      state.loading = false;
+      state.products = [action.payload, ...state.products];
+    },
+    createProductFalied(state) {
+      state.loading = false;
+    },
+
+    getAllProductsRequest(state) {
+      state.loading = true;
+    },
+    getAllProductsSuccess(state, action) {
+      state.loading = false;
+      state.products = action.payload.products;
+      state.totalProducts = action.payload.totalProducts;
+    },
+    getAllProductsFalied(state) {
+      state.loading = false;
+    },
+
+    updateProductRequest(state) {
+      state.loading = true;
+    },
+    updateProductSuccess(state, action) {
+      state.loading = false;
+      state.products = action.products.map((product) =>
+        product.id === action.payload.id ? action.payload : product,
+      );
+    },
+    updateProductFalied(state) {
+      state.loading = false;
+    },
+
+    deleteProductRequest(state) {
+      state.loading = true;
+    },
+    deleteProductSuccess(state, action) {
+      state.loading = false;
+      state.products = state.products.filter(
+        (product) => product.id !== action.payload,
+      );
+      state.totalProducts = Math.max(0, state.totalProducts - 1);
+    },
+    deleteProductFalied(state) {
+      state.loading = false;
+    },
+  },
 });
+
+export const createNewProduct = (data) => async (dispatch) => {
+  dispatch(productSlice.actions.createProductRequest());
+  await axiosInstance
+    .post("/product/admin/create", data)
+    .then((res) => {
+      dispatch(productSlice.actions.createProductSuccess(res.data.product));
+      toast.success(res.data.message || "Product created successfuly.");
+      dispatch(toggleCreateProdcutModal());
+    })
+    .catch((error) => {
+      dispatch(productSlice.actions.createProductFalied());
+      toast.error(error.response?.data?.message || "Failed to create product.");
+    });
+};
+
+export const fetchAllProducts = (page) => async (dispatch) => {
+  dispatch(productSlice.actions.getAllProductsRequest());
+  await axiosInstance
+    .get(`/product?page=${page || 1}`)
+    .then((res) => {
+      dispatch(productSlice.actions.getAllProductsSuccess(res.data));
+    })
+    .catch((error) => {
+      dispatch(productSlice.actions.getAllProductsFalied());
+    });
+};
+
+export const updateProduct = (data, id) => async (dispatch) => {
+  dispatch(productSlice.actions.updateProductRequest());
+  await axiosInstance
+    .put(`/product/admin/update/${id}`, data)
+    .then((res) => {
+      dispatch(
+        productSlice.actions.updateProductSuccess(res.data.updatedProduct),
+      );
+      toast.success(res.data.message || "Product updated successfuly");
+      dispatch(toggleUpdateProductModal());
+    })
+    .catch((error) => {
+      dispatch(productSlice.actions.updateProductFalied());
+      toast.error(error.response?.data?.message || "Failed to update product.");
+    });
+};
+
+export const deleteProduct = () => async (dispatch) => {};
 
 export default productSlice.reducer;
