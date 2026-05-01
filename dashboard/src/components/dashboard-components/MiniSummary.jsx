8e59638c606b7125ae605864b7e92f1cf1109de6
Wn0fbg <temakozlovofficiall@gmail.com>
2026-05-01 13:21:03 +0300

    finished dashboard

--- dashboard/src/components/dashboard-components/MiniSummary.jsx
+++ dashboard/src/components/dashboard-components/MiniSummary.jsx
@@ -10,7 +10,77 @@ import {
 import { useSelector } from "react-redux";
 
 const MiniSummary = () => {
-  return <></>;
+  const {
+    topSellingProducts,
+    lowStockProducts,
+    revenueGrowth,
+    newUsersThisMonth,
+    currentMonthSales,
+    orderStatusCounts,
+  } = useSelector((state) => state.admin);
+
+  let totalOrders = 0;
+  totalOrders = Object.values(orderStatusCounts).reduce(
+    (acc, count) => acc + count,
+    0,
+  );
+
+  const summary = [
+    {
+      text: "Total sales this month",
+      subText: `This mont's sales: ${currentMonthSales}`,
+      icon: <Wallet className="text-green-600" />,
+    },
+    {
+      text: "Total orders placed",
+      subText: `total orders placed: ${totalOrders}`,
+      icon: <PackageCheck className="text-blue-600" />,
+    },
+    {
+      text: "Top selling product",
+      subText: `best seller: ${topSellingProducts[0]?.name} (${topSellingProducts[0]?.total_sold} sold)`,
+      icon: <TrendingUp className="text-emerald-600" />,
+    },
+    {
+      text: "Low stock alerts",
+      subText: `${lowStockProducts} products running low on stock`,
+      icon: <AlertTriangle className="text-red-600" />,
+    },
+    {
+      text: "Revenue growth rate",
+      subText: `revenue ${revenueGrowth.includes("+") ? "up" : "down"} by ${revenueGrowth} compares to last month`,
+      icon: <BarChart4 className="text-purple-600" />,
+    },
+    {
+      text: "New customers this month",
+      subText: `New customers joined: ${newUsersThisMonth}`,
+      icon: <UserPlus className="text-yellow-600" />,
+    },
+  ];
+
+  return (
+    <>
+      <div className="bg-white rounded-xl p-6 shadow-md">
+        <h2 className="text-lg font-semibold mb-2">Summary</h2>
+        <p className="text-sm text-gray-500 mb-4">
+          Summary of key metrics for the current month.
+        </p>
+        <div className="space-y-4">
+          {summary.map((item, index) => {
+            return (
+              <div key={index} className="flex items-center space-x-3">
+                {item.icon}
+                <div>
+                  <p className="text-sm">{item.text}</p>
+                  <p className="text-sm text-gray-500">{item.subText}</p>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default MiniSummary;
